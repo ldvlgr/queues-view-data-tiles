9235ba8392c8bb994d9ac2c268466a68f3b3bcf6
Ludo Vlieger <lvlieger@twilio.com>
2023-01-19 09:50:51 -0500

    Updated activity util

--- plugin-dashboards/src/utils/ActivityUtil.js
+++ plugin-dashboards/src/utils/ActivityUtil.js
@@ -1,23 +1,32 @@
+import { Manager } from '@twilio/flex-ui';
 
-export function getAgentActivityCounts (workers = [], teams = []) {
-  let activityCounts = {};
+const _manager = Manager.getInstance();
+const workerActivities = _manager.store.getState().flex?.worker?.activities || new Map();
 
-  //All Teams, init counts
-  activityCounts["Other"] = { teamName: "Other", Available: 0, Unavailable: 0, Break: 0, Offline: 0 };
-  teams.forEach((team) => {
-      activityCounts[team] = { teamName: team, Available: 0, Unavailable: 0, Break: 0, "Clocked Out": 0 };
-      workers.forEach((wk) => {
-          let activity = wk.worker.activityName;
-          let tm = wk.worker?.attributes?.team_name || "Other";
-          if (team == tm) {
-              let count = activityCounts[tm][activity] ? activityCounts[tm][activity] : 0;
-              activityCounts[tm][activity] = count + 1;
-          } else {
-              let count = activityCounts.Other[activity] ? activityCounts.Other[activity] : 0;
-              activityCounts.Other[activity] = count + 1;
-          }
-      });
-  });
-  return activityCounts;
-  
+export function getAgentActivityCounts(workers = [], teams = []) {
+    let activityCounts = {};
+    //All Teams, init activity counts
+    activityCounts["Other"] = { teamName: "Other" };
+    workerActivities.forEach((value, key) => {
+        activityCounts["Other"][value.name] = 0;
+    });
+    teams.forEach((team) => {
+        activityCounts[team] = { teamName: team };
+        workerActivities.forEach((value, key) => {
+            activityCounts[team][value.name] = 0;
+        });
+        workers.forEach((wk) => {
+            let activity = wk.worker.activityName;
+            let tm = wk.worker?.attributes?.team_name || "Other";
+            if (team == tm) {
+                let count = activityCounts[tm][activity] ? activityCounts[tm][activity] : 0;
+                activityCounts[tm][activity] = count + 1;
+            } else {
+                let count = activityCounts.Other[activity] ? activityCounts.Other[activity] : 0;
+                activityCounts.Other[activity] = count + 1;
+            }
+        });
+        //console.log('ACTIVITY COUNTS:', activityCounts);
+    });
+    return activityCounts;
 }
