b7d835dd2def08669427a46eece04a1b79e5b803
Ludo Vlieger <lvlieger@twilio.com>
2023-06-23 12:19:18 -0400

    Optimized aggregation

--- plugin-dashboards-v2/src/utils/ActivityUtil.js
+++ plugin-dashboards-v2/src/utils/ActivityUtil.js
@@ -40,49 +40,34 @@ export function getAgentStatusCounts(workers = [], teams = []) {
     //If task count == 0, Status = "Idle"
     //If task count > 0, Status = "Busy" 
     let activityCounts = {};
-    //All Teams, init activity counts
-    
+    activityCounts["All"] = { teamName: "All" };
+    //Init activity counts
     teams.forEach((team) => {
         activityCounts[team] = { teamName: team };
         workerActivities.forEach((value, key) => {
             activityCounts[team][value.name] = 0;
+            activityCounts["All"][value.name] = 0;
         });
-        //Aggregate Activity/Status by Team
-        workers.forEach((wk) => {
-            let workerStatus = wk.worker.activityName;
-            let tm = wk.worker?.attributes?.team_name || "Other";
-            if (team == tm) {
-                if (workerStatus === STATUS_AVAILABLE) {
-                    // Determine Busy status (1+ tasks) vs. Idle (0 tasks)
-                    const tasks = wk?.tasks || [];
-                    workerStatus = STATUS_IDLE;
-                    if (tasks.length > 0) workerStatus = STATUS_BUSY
-                }
-                let count = activityCounts[tm][workerStatus] ? activityCounts[tm][workerStatus] : 0;
-                activityCounts[tm][workerStatus] = count + 1;
-            } 
-        });
-
-    });
-    //Aggregate Activity/Status for all workers
-    activityCounts["All"] = { teamName: "All" };
-    workerActivities.forEach((value, key) => {
-        activityCounts["All"][value.name] = 0;
     });
 
+    //Aggregate Activity/Status by Team
     workers.forEach((wk) => {
         let workerStatus = wk.worker.activityName;
+        let tm = wk.worker?.attributes?.team_name || "Other";
         if (workerStatus === STATUS_AVAILABLE) {
-            // Break out Busy (1+ tasks) vs.  Idle (0 tasks)
+            // Determine Busy status (1+ tasks) vs. Idle (0 tasks)
             const tasks = wk?.tasks || [];
             workerStatus = STATUS_IDLE;
             if (tasks.length > 0) workerStatus = STATUS_BUSY
         }
+        if (teams.includes(tm)) {   
+            let count = activityCounts[tm][workerStatus] ? activityCounts[tm][workerStatus] : 0;
+            activityCounts[tm][workerStatus] = count + 1;
+        }
+        // Total Count for All Workers/Teams
         let count = activityCounts.All[workerStatus] ? activityCounts.All[workerStatus] : 0;
         activityCounts.All[workerStatus] = count + 1;
     });
-    //console.log('ACTIVITY COUNTS:', activityCounts);
-
 
     return activityCounts;
 }
